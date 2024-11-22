c16bdc3c8f7a204119103795f504cc3be4890470
Artem Khazanov <tulenka04@gmail.com>
2024-11-22 13:55:43 +0200

    update classwork solution

--- src/main/java/telran/multithreading/Main.java
+++ src/main/java/telran/multithreading/Main.java
@@ -3,13 +3,64 @@ package telran.multithreading;
 import telran.view.*;
 
 public class Main {
-    public static void main(String[] args) {
-      InputOutput io = new StandardInputOutput();
-      int nRacers = io.readDouble("Enter amount of Racers: ", "Wrong amount of Racres").intValue();
-      int distance = io.readDouble("Enter distance of Race: ", "Wrong distance of Race").intValue();
-      Race race = new Race(distance);
-      race.setUp(nRacers);
-      race.startRace();
-      race.showChampion();
+  private static final int MAX_THREADS = 10;
+  private static final int MIN_THREADS = 2;
+  private static final int MIN_DISTANCE = 100;
+  private static final int MAX_DISTANCE = 3500;
+  private static final int MIN_SLEEP = 2;
+  private static final int MAX_SLEEP = 5;
+
+  public static void main(String[] args) {
+
+    InputOutput io = new StandardInputOutput();
+    Item[] items = getItems();
+    Menu menu = new Menu("Race Game", items);
+    menu.perform(io);
+
+  }
+
+  private static Item[] getItems() {
+    Item[] res = {
+        Item.of("Start new game", Main::startGame),
+        Item.ofExit()
+    };
+    return res;
+  }
+
+  static void startGame(InputOutput io) {
+    int nThreads = io.readNumberRange("Enter number of the racers", "Wrong number of the racers",
+        MIN_THREADS, MAX_THREADS).intValue();
+    int distance = io.readNumberRange("Enter distance",
+        "Wrong Distance", MIN_DISTANCE, MAX_DISTANCE).intValue();
+    Race race = new Race(distance, MIN_SLEEP, MAX_SLEEP);
+    Racer[] racers = new Racer[nThreads];
+    startRacers(racers, race);
+    joinRacers(racers);
+    displayWinner(race);
+  }
+
+  private static void displayWinner(Race race) {
+    System.out.println("Congratulations to Racer " + race.getWinner());
+
+  }
+
+  private static void joinRacers(Racer[] racers) {
+    for (int i = 0; i < racers.length; i++) {
+      try {
+        racers[i].join();
+      } catch (InterruptedException e) {
+
+      }
     }
+
+  }
+
+  private static void startRacers(Racer[] racers, Race race) {
+    for (int i = 0; i < racers.length; i++) {
+      racers[i] = new Racer(race, i + 1);
+      racers[i].start();
+    }
+
+  }
+
 }
--- src/main/java/telran/multithreading/Racer.java
+++ src/main/java/telran/multithreading/Racer.java
@@ -1,28 +1,29 @@
 package telran.multithreading;
 
+import java.util.Random;
+
 public class Racer extends Thread {
     private Race race;
     private int number;
-    private int position = 0;
 
     public Racer(Race race, int number) {
         this.race = race;
         this.number = number;
     }
 
+    @Override
     public void run() {
-        while (position < race.getDistance()) {
-            stepForwardAndWait();
-            race.winner.set(number);
-            System.out.printf("Now first is number %d \n", number);
+        int minSleep = race.getMinSleep();
+        int maxSleep = race.getMaxSleep();
+        int distance = race.getDistance();
+        Random random = new Random();
+        for (int i = 0; i < distance; i++) {
+            try {
+                sleep(random.nextInt(minSleep, maxSleep + 1));
+                System.out.printf("%d - step %d\n", number, i);
+            } catch (InterruptedException e) {
+            }
         }
-    }
-
-    private void stepForwardAndWait() {
-        position++;
-        try {
-            sleep(race.getSleepTimeOut());
-        } catch (InterruptedException e) {
-        }
+        race.winner.compareAndSet(-1, number);
     }
 }
